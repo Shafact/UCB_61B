ff32f3f0efe13efaaa313d8853eb20ba8d6565d9
Sharon <sharon.xzhou9@gmail.com>
2022-10-13 22:10:02 -0400

    Add 1.1 Practices

---
+++ HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        while (sum < 46) {
+            System.out.print(5 + 10);
+            x = x + 1;
+            sum = sum + x;
+        }
+    }
+}
